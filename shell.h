48eccebe068dfdc3f28a559751d51b9e408f192a
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-08-17 19:09:16 +0300

    simple shell

--- shell.h
+++ shell.h
@@ -98,13 +98,13 @@ int _strncmp(const char *s1, const char *s2, size_t n);
 
 /* Builtins */
 int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_env(char **args, char __attribute__((__unused__)) **front);
-int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_alias(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
+int alxshell_exit(char **args, char **front);
+int alxshell_env(char **args, char __attribute__((__unused__)) **front);
+int alxshell_setenv(char **args, char __attribute__((__unused__)) **front);
+int alxshell_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int alxshell_cd(char **args, char __attribute__((__unused__)) **front);
+int alxshell_alias(char **args, char __attribute__((__unused__)) **front);
+int alxshell_help(char **args, char __attribute__((__unused__)) **front);
 
 /* Builtin Helpers */
 char **_copyenv(void);
